a62d1f23b15c9da62033d6afd00785c7ca6eb115
Areej Fatima <areej4761@gmail.com>
2021-11-24 22:47:42 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace question52
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine(i = 10);
+            Console.ReadLine();
+        }
+    }
+}
